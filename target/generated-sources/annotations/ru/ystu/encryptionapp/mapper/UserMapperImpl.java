6f8a17dec3de2970fec91c80cef35cf5c30f2d4b
Ilya Kuzmenko <ikuzmenko@solit-clouds.ru>
2024-01-07 14:32:14 +0300

    Commit for remote use

---
+++ target/generated-sources/annotations/ru/ystu/encryptionapp/mapper/UserMapperImpl.java
@@ -0,0 +1,33 @@
+package ru.ystu.encryptionapp.mapper;
+
+import javax.annotation.processing.Generated;
+import ru.ystu.encryptionapp.dto.UserDTO;
+import ru.ystu.encryptionapp.entity.UserEntity;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-07T14:28:55+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 20.0.2 (Oracle Corporation)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDTO userToUserDTO(UserEntity user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDTO userDTO = new UserDTO();
+
+        if ( user.getId() != null ) {
+            userDTO.setId( user.getId().toString() );
+        }
+        userDTO.setName( user.getName() );
+        userDTO.setUsername( user.getUsername() );
+        userDTO.setEmail( user.getEmail() );
+
+        userDTO.setJoinDate( localDateToString(user.getJoinDate()) );
+
+        return userDTO;
+    }
+}
