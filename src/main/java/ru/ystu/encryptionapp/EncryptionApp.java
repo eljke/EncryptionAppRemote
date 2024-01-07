52e9b570bdcf4a47418eb1f50cf9022afc9e8e45
Ilya Kuzmenko <ikuzmenko@solit-clouds.ru>
2024-01-07 17:01:47 +0300

    Configuration upd

--- src/main/java/ru/ystu/encryptionapp/EncryptionApp.java
+++ src/main/java/ru/ystu/encryptionapp/EncryptionApp.java
@@ -1,13 +1,21 @@
 package ru.ystu.encryptionapp;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class EncryptionApp {
+	@Value("${app.hostingAddress}")
+	private String hostingAddress;
 
 	public static void main(String[] args) {
-		SpringApplication.run(EncryptionApp.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EncryptionApp.class, args);
+		EncryptionApp app = context.getBean(EncryptionApp.class);
+		log.info("App started on address: {}", app.hostingAddress);
 	}
 
 }
